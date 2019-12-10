b13fe03d6779dc9eaefa20513331b0f8b281279f
VenomOcelot <faddaoui.i@gmail.com>
2019-12-10 20:21:07 +0100

    first commit initialization of the project

---
+++ src/main/java/com/esipe/clemsilies/tpmodrealav/TpModrealavApplication.java
@@ -0,0 +1,12 @@
+package com.esipe.clemsilies.tpmodrealav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpModrealavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpModrealavApplication.class, args);
+	}
+}
---
+++ src/test/java/com/esipe/clemsilies/tpmodrealav/TpModrealavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esipe.clemsilies.tpmodrealav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpModrealavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
